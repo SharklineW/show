49ef784c1a6244e90e85335068aa23ee6d4ba986
吴伟泽share <2225490296@qq.com>
2021-09-23 23:33:07 +0800

    完成了登录功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name:'Home',
+    methods:{
+        logout(){
+            //清空token
+            window.sessionStorage.clear()
+            //跳转到登录页
+            this.$router.push('/login')
+        }
+    }
+
 }
 </script>
+
+<style lang="less" scoped>
+
+</style>
